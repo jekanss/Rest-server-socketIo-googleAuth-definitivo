d636a596a1018bd14d280d08d175b217acc0546d
jeanchm94@gmail.com <jeanchm94@gmail.com>
2022-05-11 19:00:59 -0500

    React 17 typescript tailwind definitivo

--- Desktop/React17-Cascaron-App-main/tailwind.config.js
+++ Desktop/React17-Cascaron-App-main/tailwind.config.js
@@ -5,5 +5,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  variants: { 
+    animation: ['responsive', 'hover', 'group-hover'],
+    animate: ['responsive', 'hover', 'group-hover'],    
+    fontSize: ['responsive', 'hover', 'group-hover'],    
+    transform: ['responsive', 'hover', 'group-hover'],    
+    scale: ['responsive', 'hover', 'group-hover'] ,   
+    margin: ['responsive', 'hover', 'group-hover'] ,   
+    padding: ['responsive', 'hover', 'group-hover'],   
+    visibility: ['responsive', 'hover', 'group-hover'],
+    display:['responsive', 'hover', 'group-hover'],
+    borderWidth: ['hover', 'focus', 'group-hover'],
+    aspectRatio: ['responsive', 'hover', 'group-hover'],
+    active: ['responsive', 'hover', 'group-hover']
+  },
   plugins: [],
 }
